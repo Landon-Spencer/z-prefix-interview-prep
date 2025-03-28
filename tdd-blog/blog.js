3225e905f0838a73513e89b158de82acacef652d
Landon-Spencer <landon.r.spencer@gmail.com>
2025-03-28 14:38:00 -0600

    finished calculator

--- tdd-blog/blog.js
+++ tdd-blog/blog.js
@@ -1,8 +1,123 @@
 class Blog {
   constructor() {
-
+    this.posts = [];
   }
 
+  getBlogPosts () {
+    let result;
+    if (this.posts.length == 0) {
+      result = `There are ${this.posts.length} blog posts`
+    } else {
+      result = this.posts.map((post) => post.title);
+    }
+    return result
+  }
+
+  addBlogPost (newPost, newAuthor = false, newGenre) {
+
+    let postObject = {
+      title: newPost,
+      author: newAuthor,
+      genre: newGenre,
+      comments: []
+    }
+
+    // if (!postObject.author) {
+    //   postObject.author = false;
+    // } else {
+    //   postObject.authors.push(newAuthor);
+    // }
+
+    this.posts.push(postObject);
+  }
+
+  getBlogAuthors () {
+    let result = this.posts.map((post) => post.author)
+    return result;
+  }
+
+  getSpecificBlogAuthor (blogPost) {
+    let authorPost = this.posts.filter((post) => post.title == blogPost);
+    // console.log(authorPost);
+    let postAuthor = authorPost[0]?.author;
+    // console.log(postAuthor);
+    if (typeof postAuthor == 'string') {
+      return postAuthor;
+    } else if (postAuthor == false) {
+      return 'Blog has no author';
+    } else {
+      return 'No such blog post';
+    }
+  }
+
+  getBlogsFromGenre (genre) {
+    let allPost = [];
+    let genrePost = this.posts.filter((post) => post.genre == genre);
+    genrePost.map((post) => {
+      let filteredPost = {
+        title: post.title,
+        author: post.author,
+        genre: post.genre
+      }
+      allPost.push(filteredPost);
+    })
+    return allPost;
+  }
+
+  getAllGenres () {
+    let result = this.posts.map((post) => post.genre);
+    if (result.length == 0) {
+      return "There are no genres available"
+    } else {
+      return result;
+    }
+  }
+
+  getComments (title) {
+    let commentPost = this.posts.filter((post) => post.title == title);
+    // console.log("getComments", commentPost[0].comments)
+    return commentPost[0].comments;
+  }
+
+  getBlogComments (title) {
+    if (this.getComments(title).length == 0) {
+      return this.getComments(title)
+    } else {
+      // console.log(this.getComments(title));
+      let result = [{comments: this.getComments(title)}];
+      return result;
+    }
+  }
+
+  addComment (title, user, comment) {
+    let newComment = {
+      user: user,
+      comment: comment
+    }
+    this.getComments(title).push(newComment);
+    // console.log("addComment", this.getComments(title));
+  }
+
+  getCommentsBy (username) {
+    let result = {
+      comments: [],
+      user: username
+    };
+    this.posts.forEach((post) => {
+      this.getComments(post.title).forEach((comment) => {
+        if (comment.user == username) {
+          result.comments.push({article: post.title, comment: comment.comment});
+        }
+      })
+    })
+    return result;
+  }
+
+  deleteBlog (title) {
+    let allPosts = this.getBlogPosts();
+    let index = allPosts.indexOf(title);
+    this.posts.splice(index, 1);
+  }
 
 }
 
--- tdd-calculator/calculator.js
+++ tdd-calculator/calculator.js
@@ -1,7 +1,61 @@
 class Calculator {
   constructor() {
-    
+    this.state = 0;
   }
+
+  getCurrentState () {
+    return this.state;
+  }
+
+  addInputs (...numbers) {
+    this.state = 0;
+    // console.log(numbers);
+    numbers.forEach((number) => {
+      this.state += number;
+      // console.log(`current state ${this.state} and number added ${number}`);
+    });
+    return this.state;
+  }
+
+  multiplyInputs (...numbers) {
+    this.state = 1;
+    numbers.forEach((number) => this.state *= number);
+    return this.state;
+  }
+
+  divideInputs (a, b = 1) {
+    return a / b;
+  }
+
+  subtractInputs (...numbers) {
+    this.state = numbers[0];
+    numbers.splice(0, 1);
+    numbers.forEach((number) => this.state -= number);
+    return this.state;
+  }
+
+  toPower (a, b) {
+    return a ** b;
+  }
+
+  squareRoot (a) {
+    return Math.round((Math.sqrt(a) * 100)) / 100;
+  }
+
+  clearCurrentStatus () {
+    this.state = 0;
+    return true;
+  }
+
+  negPosReversal (a) {
+    if (a > 0) {
+      return a - (2 * a);
+    } else {
+      return Math.abs(a);
+    }
+  }
+
+
 }
 
 module.exports = Calculator
