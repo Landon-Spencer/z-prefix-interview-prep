eb40556664df676345e372cfcdbeb537a1010e70
Landon-Spencer <landon.r.spencer@gmail.com>
2025-03-28 15:01:41 -0600

    working on set

--- tdd-set-javascript/set.js
+++ tdd-set-javascript/set.js
@@ -1,7 +1,21 @@
 class GalvanizeSet {
   constructor() {
-
+    this.items = [];
   }
+
+  getSize () {
+    return this.items.length;
+  }
+
+  insert (item) {
+    this.items.push(item);
+    return this.items.length;
+  }
+
+  contains (item) {
+    return this.items.includes(item);
+  }
+
 }
 
 module.exports = GalvanizeSet
--- tdd-set-javascript/spec/set.test.js
+++ tdd-set-javascript/spec/set.test.js
@@ -6,60 +6,60 @@ describe('Set', () => {
 
     expect(set.getSize()).toEqual(0)
   })
-  //
-  // it('allows you to insert an item', () => {
-  //   let set = new GalvanizeSet()
-  //
-  //   set.insert(1)
-  //   expect(set.getSize()).toEqual(1)
-  // })
-  //
-  // it('allows you to insert multiple items', () => {
-  //   let set = new GalvanizeSet()
-  //   let setSize = 10
-  //
-  //   for (let i = 0; i < setSize; i++) {
-  //     set.insert(i)
-  //   }
-  //
-  //   expect(set.getSize()).toEqual(setSize)
-  // })
-  //
-  // it('allows you to check for contents when empty', () => {
-  //   let set = new GalvanizeSet()
-  //
-  //   expect(set.contains(1)).toEqual(false)
-  // })
-  //
-  // it('allows you to check for for missing contents', () => {
-  //   let set = new GalvanizeSet()
-  //   let setSize = 10
-  //
-  //   for (let i = 0; i < setSize; i++) {
-  //     set.insert(i)
-  //   }
-  //
-  //   expect(set.contains(11)).toEqual(false)
-  // })
-  //
-  // it('allows you to check for contents', () => {
-  //   let set = new GalvanizeSet()
-  //   let setSize = 10
-  //
-  //   for (let i = 0; i < setSize; i++) {
-  //     set.insert(i)
-  //   }
-  //
-  //   expect(set.contains(1)).toEqual(true)
-  // })
-  //
-  // it('tells you a deletion failed on an empty set', () => {
-  //   let set = new GalvanizeSet()
-  //   const deletionResult = set.delete(1)
-  //   expect(deletionResult).toEqual(false)
-  //   expect(set.getSize()).toEqual(0)
-  // })
-  //
+
+  it('allows you to insert an item', () => {
+    let set = new GalvanizeSet()
+
+    set.insert(1)
+    expect(set.getSize()).toEqual(1)
+  })
+
+  it('allows you to insert multiple items', () => {
+    let set = new GalvanizeSet()
+    let setSize = 10
+
+    for (let i = 0; i < setSize; i++) {
+      set.insert(i)
+    }
+
+    expect(set.getSize()).toEqual(setSize)
+  })
+
+  it('allows you to check for contents when empty', () => {
+    let set = new GalvanizeSet()
+
+    expect(set.contains(1)).toEqual(false)
+  })
+
+  it('allows you to check for for missing contents', () => {
+    let set = new GalvanizeSet()
+    let setSize = 10
+
+    for (let i = 0; i < setSize; i++) {
+      set.insert(i)
+    }
+
+    expect(set.contains(11)).toEqual(false)
+  })
+
+  it('allows you to check for contents', () => {
+    let set = new GalvanizeSet()
+    let setSize = 10
+
+    for (let i = 0; i < setSize; i++) {
+      set.insert(i)
+    }
+
+    expect(set.contains(1)).toEqual(true)
+  })
+
+  it('tells you a deletion failed on an empty set', () => {
+    let set = new GalvanizeSet()
+    const deletionResult = set.delete(1)
+    expect(deletionResult).toEqual(false)
+    expect(set.getSize()).toEqual(0)
+  })
+
   // it('tells you deletion failed if an item is not present', () => {
   //   let set = new GalvanizeSet()
   //   let setSize = 10
